61848cc790db1e2e786e7407fb544eb831d43c69
Naruf <102542599+Naruf@users.noreply.github.com>
2026-08-10 13:50:36 +0200

    Footer added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,33 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Nadia's weather app</div>;
+  return (
+    <div className="App">
+      <header>
+        <h1>Nadia's weather app</h1>
+      </header>
+      <footer>
+        <div className="Footer">
+          This project was coded by{" "}
+          <a href="https://github.com/Naruf" target="_blank">
+            Nadia Ruiz
+          </a>
+          , is{" "}
+          <a
+            href="https://github.com/Naruf/react-weather-app-final-project"
+            target="_blank"
+          >
+            open-sourced on Github
+          </a>{" "}
+          and hosted on{" "}
+          <a
+            href="https://react-weather-app-final-project.vercel.app/"
+            target="_blank"
+          >
+            Vercel
+          </a>
+        </div>
+      </footer>
+    </div>
+  );
 }
