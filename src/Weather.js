0631d4d6df5a287a406615463812109c39f04066
Naruf <102542599+Naruf@users.noreply.github.com>
2026-08-14 18:13:03 +0200

    Autofocus set off

--- src/Weather.js
+++ src/Weather.js
@@ -56,7 +56,7 @@ export default function Weather({ defaultCity }) {
                   type="search"
                   placeholder="Enter a city name..."
                   required
-                  autoFocus="on"
+                  // autoFocus="on"
                   onChange={handleCityUpdate}
                 />
               </div>
